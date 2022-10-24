4f06ff7af5ea828bf6fd5737f1c3f8963de7096e
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-24 22:38:43 +0200

    Google and github authentication added

--- src/Pages/Authentication/SignUp.jsx
+++ src/Pages/Authentication/SignUp.jsx
@@ -1,3 +1,4 @@
+import { GithubAuthProvider, GoogleAuthProvider } from "firebase/auth";
 import React, { useContext, useState } from "react";
 import { MdClose } from "react-icons/md";
 import { Link } from "react-router-dom";
@@ -11,7 +12,7 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
 
   // ** AuthContext
-  const { createUser } = useContext(AuthContext);
+  const { createUser, socialSignIn } = useContext(AuthContext);
   // ** event handlers
   const emailHandler = (event) => {
     const emailOutput = event.target.value;
@@ -65,16 +66,46 @@ const SignUp = () => {
         form.reset();
         toast.success("User logged in", {
           position: "top-center",
-          autoClose: 5000,
+          autoClose: 500,
         });
       } catch (error) {
         toast.error(error.message, {
           position: "top-right",
-          autoClose: 5000,
+          autoClose: 500,
         });
       }
     };
     emailPassUser();
+  };
+  // ** Google
+
+  const googleProvider = new GoogleAuthProvider();
+  const googleHandler = () => {
+    const loginGoogle = async () => {
+      try {
+        await socialSignIn(googleProvider);
+        toast.success("User logged in");
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+    loginGoogle();
+  };
+
+  // ** gitHub
+
+  const githubProvider = new GithubAuthProvider();
+
+  const githubHandler = () => {
+    const loginGithub = async () => {
+      try {
+        await socialSignIn(githubProvider);
+        toast.success("User logged in");
+      } catch (error) {
+        toast.error(error.message);
+      }
+    };
+    loginGithub();
   };
 
   return (
@@ -209,7 +240,11 @@ const SignUp = () => {
           <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
         </div>
         <div className="flex justify-center space-x-4">
-          <button aria-label="Log in with Google" className="p-3 rounded-sm">
+          <button
+            onClick={googleHandler}
+            aria-label="Log in with Google"
+            className="p-3 rounded-sm"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 32 32"
@@ -227,7 +262,11 @@ const SignUp = () => {
               <path d="M31.937 6.093c-1.177 0.516-2.437 0.871-3.765 1.032 1.355-0.813 2.391-2.099 2.885-3.631-1.271 0.74-2.677 1.276-4.172 1.579-1.192-1.276-2.896-2.079-4.787-2.079-3.625 0-6.563 2.937-6.563 6.557 0 0.521 0.063 1.021 0.172 1.495-5.453-0.255-10.287-2.875-13.52-6.833-0.568 0.964-0.891 2.084-0.891 3.303 0 2.281 1.161 4.281 2.916 5.457-1.073-0.031-2.083-0.328-2.968-0.817v0.079c0 3.181 2.26 5.833 5.26 6.437-0.547 0.145-1.131 0.229-1.724 0.229-0.421 0-0.823-0.041-1.224-0.115 0.844 2.604 3.26 4.5 6.14 4.557-2.239 1.755-5.077 2.801-8.135 2.801-0.521 0-1.041-0.025-1.563-0.088 2.917 1.86 6.36 2.948 10.079 2.948 12.067 0 18.661-9.995 18.661-18.651 0-0.276 0-0.557-0.021-0.839 1.287-0.917 2.401-2.079 3.281-3.396z"></path>
             </svg>
           </button>
-          <button aria-label="Log in with GitHub" className="p-3 rounded-sm">
+          <button
+            onClick={githubHandler}
+            aria-label="Log in with GitHub"
+            className="p-3 rounded-sm"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 32 32"
