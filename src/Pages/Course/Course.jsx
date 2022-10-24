6588226370294f8ebba2488dd4fb774392355400
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-24 23:36:11 +0200

    Course information added

--- src/Pages/Course/Course.jsx
+++ src/Pages/Course/Course.jsx
@@ -1,102 +1,33 @@
 import React from "react";
+import { MdOutlineCategory, MdOutlineHourglassTop } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 const Course = ({ course }) => {
   const { title, price, image, category, duration } = course;
 
   return (
-    <>
-      <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
-        <a href="/" aria-label="Article">
-          <img
-            src={image}
-            className="object-cover w-full h-64 rounded"
-            alt=""
-          />
-        </a>
+    <Link to="/">
+      <div className="overflow-hidden transition-shadow duration-300 bg-white  hover:bg-sky-400 hover:shadow-2xl p-3 rounded-2xl ">
+        <img src={image} className="object-cover w-full h-64 rounded" alt="" />
+
         <div className="py-5">
-          <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
-            13 Jul 2020
+          <p className="mb-2 text-xs font-semibold text-gray-600 uppercase flex items-center">
+            <MdOutlineHourglassTop className="text-blue-600 w-4 h-4" />
+            <small>Duration: {duration} hrs</small>
           </p>
-          <a
-            href="/"
-            aria-label="Article"
-            className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
-          >
-            <p className="text-2xl font-bold leading-5">Diving to the deep</p>
-          </a>
-          <p className="mb-4 text-gray-700">
-            Sed ut perspiciatis unde omnis iste natus error sit sed quia
-            consequuntur magni voluptatem doloremque.
+
+          <p className="text-2xl font-bold leading-2">
+            {title && title.slice(0, 30)} ...
           </p>
-          <div className="flex space-x-4">
-            <a
-              href="/"
-              aria-label="Likes"
-              className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
-            >
-              <div className="mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-                >
-                  <polyline
-                    points="6 23 1 23 1 12 6 12"
-                    fill="none"
-                    strokeMiterlimit="10"
-                  />
-                  <path
-                    d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeMiterlimit="10"
-                  />
-                </svg>
-              </div>
-              <p className="font-semibold">7.4K</p>
-            </a>
-            <a
-              href="/"
-              aria-label="Comments"
-              className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
-            >
-              <div className="mr-2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-                >
-                  <polyline
-                    points="23 5 23 18 19 18 19 22 13 18 12 18"
-                    fill="none"
-                    strokeMiterlimit="10"
-                  />
-                  <polygon
-                    points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeMiterlimit="10"
-                  />
-                </svg>
-              </div>
-              <p className="font-semibold">81</p>
-            </a>
+
+          <p className="mb-4 text-gray-700 font-semibold">$ {price}</p>
+          <div className="flex items-center">
+            <MdOutlineCategory className="text-orange-400 mr-2" />
+            <small className="font-semibold ">{category}</small>
           </div>
         </div>
       </div>
-    </>
+    </Link>
   );
 };
 
--- src/Pages/Courses/Courses.jsx
+++ src/Pages/Courses/Courses.jsx
@@ -6,9 +6,12 @@ const Courses = () => {
   const courses = useLoaderData();
 
   return (
-    <div className="grid lg:grid-cols-12">
-      <div className="col-start-1 col-end-10">
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div className="grid lg:grid-cols-12 ">
+      <div className="lg:col-start-1 lg:col-end-10 ">
+        <h1 className="lg:text-3xl font-extralight mt-2  text-center ">
+          Our Courses
+        </h1>
+        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 ">
           <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
             {courses.map((course) => (
               <Course key={course.id} course={course} />
@@ -16,7 +19,7 @@ const Courses = () => {
           </div>
         </div>
       </div>
-      <div className="col-start-10 col-end-12">
+      <div className="lg:col-start-10 lg:col-end-12">
         <h1>Right side</h1>
       </div>
     </div>
