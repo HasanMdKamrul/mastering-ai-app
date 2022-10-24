f07f6c450e9ad5fe0cdd282876effe68297a0976
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-10-24 20:42:13 +0200

    Signin and signup page initial setup added

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
+import SignIn from "../Pages/Authentication/SignIn";
+import SignUp from "../Pages/Authentication/SignUp";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home";
 
@@ -16,6 +18,14 @@ const router = createBrowserRouter([
       {
         path: "/home",
         element: <Home />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/signin",
+        element: <SignIn />,
       },
     ],
   },
